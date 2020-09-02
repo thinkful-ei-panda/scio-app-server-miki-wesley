ab95445e051436d5c9f02dfc839976c3925c0065
Wesley Rou <wesleyrou@gmail.com>
2020-09-01 21:41:13 -0700

    implemented guess route, still fixing tests

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -4,6 +4,122 @@ const { requireAuth } = require('../middleware/jwt-auth')
 
 const languageRouter = express.Router()
 
+class _Node {
+  constructor(value,next){
+    this.value = value;
+    this.next = next;    
+  }
+}
+
+class LinkedList {
+  constructor(){
+    this.head = null
+    this.last = null
+  }
+
+  add (item) {
+    let newNode = new _Node(item,null)
+
+    if(this.head === null) {      
+      this.head = newNode
+      this.last = newNode
+    } else {
+      this.last.next = newNode
+      this.last = newNode
+    }
+  }
+
+  remove(item){
+    if(this.head === null){
+      throw new Error('Empty Linked List')
+    }
+
+    let currentNode = this.head
+    let previousNode = this.head
+    while(currentNode.value !== item){
+      if(currentNode === null)
+        throw new Error('Could not find item to be deleted')
+      previousNode = currentNode
+      currentNode = currentNode.next
+    }
+
+    previousNode.next = currentNode.next
+    currentNode = null
+  }
+
+  update(item){
+    if(this.head === null){
+      throw new Error('Empty Linked List')
+    }
+
+    let currentNode = this.head    
+    while(currentNode.value.id !== item.id){
+      if(currentNode === null)
+        throw new Error('Could not find item to be deleted')      
+      currentNode = currentNode.next
+    }
+
+    currentNode.value = item
+  }
+
+  moveWordNode(item){
+    if(this.head === null){
+      throw new Error('Empty Linked List')
+    }
+
+    let currentNode = this.head
+    let previousNode = this.head
+    while(currentNode.value.id !== item.id){
+      if(currentNode === null)
+        throw new Error('Could not find item to be deleted')      
+      previousNode = currentNode
+      currentNode = currentNode.next
+    }
+
+    this.head = currentNode.next
+    let stepsRemaining = item.memory_value    
+
+    while(stepsRemaining > 0 && currentNode.next !== null){
+      //save current.next to set as previous
+      const newPrevious = currentNode.next      
+      
+      if(previousNode === currentNode){        
+        currentNode.next = currentNode.next.next
+        this.head.next = currentNode
+      } else {      
+        //1 previous, 2 current, 3 current.next, 4 current.next.next
+
+        //save 4th temporarily
+        let fourthNode = currentNode.next.next
+
+        //set 1.next as 3
+        previousNode.next = currentNode.next
+
+        //set 3.next as 2
+        currentNode.next.next = currentNode
+
+        //set 2.next as 4
+        currentNode.next = fourthNode
+      }      
+
+      previousNode = newPrevious
+      stepsRemaining--         
+    }
+
+    if(currentNode.next === null){
+      this.last = currentNode
+    }
+  }
+
+  display(){
+    let currentNode = this.head
+    while(currentNode !== null) {
+      console.log(currentNode.value)
+      currentNode = currentNode.next
+    }
+  }
+}
+
 languageRouter
   .use(requireAuth)
   .use(async (req, res, next) => {
@@ -26,8 +142,7 @@ languageRouter
   })
 
 languageRouter
-  .get('/', async (req, res, next) => {
-    console.log(req.language)
+  .get('/', async (req, res, next) => {    
     try {
       const words = await LanguageService.getLanguageWords(
         req.app.get('db'),
@@ -47,13 +162,98 @@ languageRouter
 languageRouter
   .get('/head', async (req, res, next) => {
     // implement me
-    res.send('implement me!')
+    let {language} = req
+    // console.log(language, user)
+    try {      
+      const headWord = await LanguageService.getWordById(
+        req.app.get('db'),
+        req.language.head,
+      )
+
+      const nextWordResponse = {
+        nextWord: headWord.original,
+        totalScore: language.total_score,
+        wordCorrectCount: headWord.correct_count,
+        wordIncorrectCount: headWord.incorrect_count,
+      }
+
+      res.status(200).json(nextWordResponse)
+      next()
+    } catch (error) {
+      next(error)
+    }
   })
 
 languageRouter
-  .post('/guess', async (req, res, next) => {
+  .post('/guess', express.json(), async (req, res, next) => {    
+    
     // implement me
-    res.send('implement me!')
+    const {guess} = req.body        
+
+    if(!guess) {
+      return res.status(400).json({ error: "Missing 'guess' in request body" })
+    }
+
+    try{
+      const words = await LanguageService.getLanguageWords(
+        req.app.get('db'),
+        req.language.id
+      )
+
+      const headWord = await LanguageService.getWordById(
+        req.app.get('db'),
+        req.language.head,
+      )
+
+      //create linked list and populate it with the words in their current order
+      let wordsList = new LinkedList
+      LanguageService.populateLinkedListWithWords(words, wordsList, headWord.id)
+
+      //initialize response with values from headWord and wordsList
+      const answer = headWord.translation, nextWord = wordsList.head.next.value.original
+      let response = {
+        answer,
+        isCorrect: null,
+        nextWord,
+        totalScore: req.language.total_score,
+        wordCorrectCount: headWord.correct_count,
+        wordIncorrectCount: headWord.incorrect_count,
+      }   
+      
+      //checks if guess matches answer
+      if(guess !== headWord.translation){        
+        headWord.incorrect_count++;
+        headWord.memory_value=1;
+        response.isCorrect = false;             
+        // ++response.wordIncorrectCount;
+      } else {
+        headWord.correct_count++;
+        headWord.memory_value=headWord.memory_value*2  
+        response.isCorrect = true;  
+        response.totalScore++;     
+        // ++response.wordCorrectCount;
+      }
+
+      //update wordList with new values, order, and next pointers
+      //update response with values      
+      wordsList.update(headWord)
+      wordsList.moveWordNode(headWord)
+      LanguageService.updateNextInLLValues(wordsList)      
+
+      //update database with new words list
+      //update database with new language head
+      await LanguageService.updateDatabaseWithWordsAndHead(
+        req.app.get('db'),
+        wordsList,
+        req.language.id
+      )
+
+      //send response to client
+      res.status(200).json(response)
+      next()      
+    } catch (error) {
+      next()
+    }
   })
 
 module.exports = languageRouter
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,3 +1,5 @@
+const languageRouter = require("./language-router")
+
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
@@ -28,6 +30,63 @@ const LanguageService = {
       )
       .where({ language_id })
   },
+
+  getWordById(db, id) {
+    return db
+      .from('word')
+      .select(
+        'id',
+        'language_id',
+        'original',
+        'translation',
+        'next',
+        'memory_value',
+        'correct_count',
+        'incorrect_count',
+        )
+        .where({ id })
+        .first()
+  },
+
+  updateWordById(db, id, updatedWord){       
+    return db
+      .from('word')
+      .where({id})      
+      .update(updatedWord)
+  },
+
+  async updateDatabaseWithWordsAndHead(db,LinkedList,language_id){    
+    let currentWord = LinkedList.head
+    while(currentWord !== null){
+      
+      await this.updateWordById(db,currentWord.value.id,currentWord.value)
+      currentWord = currentWord.next
+    }
+    
+    currentWord = LinkedList.head    
+    await db
+      .from('language')
+      .where({ id : language_id })
+      .update({head: currentWord.value.id})
+  },
+
+  populateLinkedListWithWords(words,LinkedList, head_id){
+    
+    let next_id = head_id    
+    while(next_id !== null){
+      let targetNode = words.find(word => word.id === next_id)      
+      LinkedList.add(targetNode)
+      next_id = targetNode.next
+    }
+  },
+
+  updateNextInLLValues (LinkedList){
+    let currentWord = LinkedList.head
+    while(currentWord.next !== null){
+      currentWord.value.next = currentWord.next.value.id
+      currentWord = currentWord.next
+    }
+  }
 }
 
 module.exports = LanguageService
